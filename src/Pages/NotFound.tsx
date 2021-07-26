b28fd9c63092959167f367741f73df6a796fbae4
Benjamin Zenth <benjaminzenth@icloud.com>
2021-07-26 16:57:39 +0200

    add 404 Page

--- src/Pages/NotFound.tsx
+++ src/Pages/NotFound.tsx
@@ -1,12 +1,21 @@
+
 import React from "react"
-import { Header } from "../Components"
+import { Header, Footer} from "../Components"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSadTear } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
 
 const NotFound = () => {
     return(
         <div>
             <Header/>
-            <div className="content">
+            <div className="content notFound">
+                <p className="center"><FontAwesomeIcon className="notFound_icon grey" icon={faSadTear}/></p>
+                <h1 className="red fontBold center">Wir können diese Seite leider nicht finden</h1>
+                <p className="center"><Link className="grey fontNormal underline link subheading" to="/">Zur Startseite</Link></p>
             </div>
+            <Footer/>
+
         </div>
     )
 }
