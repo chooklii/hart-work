c8d6af92bdaa2101d5e0e53744d1f9866f64fe34
Benjamin Zenth <benjaminzenth@icloud.com>
2021-08-03 13:04:49 +0200

    try

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,10 +1,13 @@
 
 import React from "react"
 import { Basics, Header, TopImage, BottomImage, Team, Footer, HomeCarousel} from "../Components"
-
+import Helmet from 'react-helmet';
 const Home = () => {
     return(
         <div>
+            <Helmet>
+                <title>#HARTWORK</title>
+            </Helmet>
             <Header/>
             <div className="content">
             <HomeCarousel/>
