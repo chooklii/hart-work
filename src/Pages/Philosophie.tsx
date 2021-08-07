9a69c0c26f7f957ea478599a5c5b0c4f5e2270c1
Benjamin Zenth <benjaminzenth@icloud.com>
2021-08-07 10:44:31 +0200

    new Philosophie

--- src/Pages/Philosophie.tsx
+++ src/Pages/Philosophie.tsx
@@ -13,16 +13,14 @@ const Philosophie = () => {
                 <h2 className="red fontBold subheading center">#Philosophie</h2>
 
                 <p className="grey fontNormal philosophie">
-                    Das Innere einer Frucht ist der Kern und dieser ist der Ursprung von etwas ganz Großem. Da ist es egal, ob es eine Traube oder eine Melone wird - der Kern ist immer das Wichtigste, sowohl bei Früchten, als auch beim menschlichen Körper. Er ist immer hart und hart ist auch das erste Wort von unserem Neuunternehmen #HARTWORK. Der zweite Teil der Unternehmensbezeichnung ist „work“. Work heißt übersetzt arbeiten. Wir wollen mit unseren Kunden durch individuelles und attraktives Personaltraining an einem gesünderen Lebensstil arbeiten. Für diese Kombination steht #HART- WORK: Gezielte Physiotherapie gepaart mit Fitnessübungen im Personaltraining und Gruppentraining.
+                    Das Innere einer Frucht ist der Kern und dieser ist der Ursprung von etwas ganz Großem. Da ist es egal, ob es eine Traube oder eine Melone wird - der Kern ist immer das Wich-tigste, sowohl bei Früchten, als auch beim menschlichen Körper. Er ist immer hart und hart ist auch das erste Wort von unserem Neuunternehmen #HARTWORK. 
                     <br /><br />
-                    Die Philosophie von #HARTWORK ist das Innere, der Ursprung und die Mitte.
+                    Der zweite Teil der Unternehmensbezeichnung ist „work“. Work heißt übersetzt arbeiten. Wir wollen mit unseren Kunden durch individuelles und attraktives Personaltraining an einem gesünderen Lebensstil arbeiten. Für diese Kombination steht #HART- WORK: Ge-zielte Physiotherapie gepaart mit Fitnessübungen im Personaltraining und Gruppentrai-ning. 
                     <br /><br />
-                    Mit der Mitte verbinden viele Menschen immer etwas Besonderes. Ob es nun die goldene Mitte im Leben ist, das mittlere Kind von dreien oder ein zentraler Punkt, von dem aller Erfolg ausgeht - die Mitte steht immer für etwas Außergewöhnliches.
+                    Die Philosophie von #HARTWORK ist das Innere, der Ursprung und die Mitte. 
                     <br /><br />
-                    Genau hier wird das Neuunternehmen #HARTWORK ansetzen. Es soll ein Unternehmen für Kunden und Patienten werden, die genau das suchen. Ein Zentrum, dass nicht nur das Oberflächliche sieht, sondern sich auf das Wichtigste fokussiert. Im menschlichen Körper ist das Zentrum, das „Core“. Core (Lateinisch) heißt übersetzt „das Kernstück“. Hier ist das Innere gemeint und nicht nur die Oberfläche, die jeder sieht.
+                    Mit der Mitte verbinden viele Menschen immer etwas Besonderes. Ob es nun die goldene Mitte im Leben ist, das mittlere Kind von dreien oder ein zentraler Punkt, von dem aller Erfolg ausgeht - die Mitte steht immer für etwas Außergewöhnliches. 
                     <br />
-                    Core ist für die Rumpfstabilität im Körper zuständig. Hier kommt es zum Zusammenspiel verschiede- ner Muskelketten, die für die notwendige Stabilität sorgen. Ohne die Stabilität des Rumpfes geht es nicht. #HARTWORK möchte, dass Patienten und Kunden ihre Mitte finden und so eine aktivere und körperbewusstere Lebensweise verwirklichen können.
-                    <br /><br />
                     Dabei ist die Kombination aus Fitness und Physiotherapie von entscheidender Bedeutung! Diese Kombination ist der Kern und die Philosophie von unserem neuen Unternehmen #HARTWORK.
                 </p>
             </div>
